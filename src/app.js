02022319dec78a361a402842a96aa65ee79c686b
Masgee <akudankaucumanteman@gmail.com>
2024-11-22 14:10:07 +0700

    perubahan

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require("cors");
+const dotenv = require('dotenv')
+const adminAuthorization = require('./middleware/adminAuthorization')
+dotenv.config();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send('Hello there!')
+})
+
+const authController = require("./auth/auth.controller");
+const itemController = require("./item/item.controller");
+const userController = require("./user/user.controller");
+const transactionController = require("./transactions/transaction.controller");
+
+app.use("/api/transactions", transactionController);
+app.use("/api/auth", authController);
+app.use("/api/items", itemController);
+app.use("/api/users", adminAuthorization, userController);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ` + PORT)
+})
+
